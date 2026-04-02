d04a930ddc7ac538ab521b94d99077090807527b
Hus11s <ilalobodanov@gmail.com>
2026-04-02 04:00:57 +0300

    Update ExampleMod.java

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -3,17 +3,20 @@ package net.fabricmc.example;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
 
 public class ExampleMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
-            MinecraftClient client = MinecraftClient.getInstance();
-            if (client.player == null) return;
-            
-            drawContext.drawCenteredTextWithShadow(client.textRenderer, "MOD ACTIVE", 
-                drawContext.getScaledWindowWidth() / 2, 10, 0xFFFFFF);
+            if (MinecraftClient.getInstance().player != null) {
+                drawContext.drawCenteredTextWithShadow(
+                    MinecraftClient.getInstance().textRenderer, 
+                    "TEST OK", 
+                    drawContext.getScaledWindowWidth() / 2, 
+                    10, 
+                    0xFFFFFF
+                );
+            }
         });
     }
 }
